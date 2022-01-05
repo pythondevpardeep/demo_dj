64398c33cdac182a578f8b23b4ed500e47225c5a
techappg <Qwert2ab@12345>
2022-01-05 17:27:24 +0530

    updated views

--- home/views.py
+++ home/views.py
@@ -3,7 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from datetime import datetime
 
+now = datetime.now()
+
+
+print("date and time =", dt_string)	
 def index(request):
-    msg = "Success new updated now 5.20 pm"
+    
+    msg = "Code Updated at" + now.strftime("%d/%m/%Y %H:%M:%S")
     return HttpResponse(msg, content_type='text/plain')
