f95573a9f91ef144bbf54868f40a1068cdd285d1
techappg <Qwert2ab@12345>
2022-01-05 15:55:52 +0530

    updated code

--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+
 def index(request):
-    msg="Success"
+    msg = "Success new"
     return HttpResponse(msg, content_type='text/plain')
